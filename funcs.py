994ad3c077dae122f609c5ad13e1bdab1985a023
NightlyOneV <joserobertomiranda.contato@gmail.com>
2024-09-17 09:30:28 -0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,20 +1,21 @@
+def main():
+    r = leNumeroInt()
+    print("numero digitado é :" , r)
+    n1 = leNumeroInt()
+    n2 = leNumeroInt()
+    res  = soma (n1 , n2)
+    print ("a soma é :" , res)
+    imprimirCaixa(10)
+
 def leNumeroInt():
     numero = input("digite um numero inteiro: ")
 
     return int(numero)
 
-r = leNumeroInt()
-print("numero digitado é :" , r)
-
 def soma (numero1 ,numero2):
     resultado = numero1 + numero2
 
     return resultado 
-
-n1 = leNumeroInt()
-n2 = leNumeroInt()
-res  = soma (n1 , n2)
-print ("a soma é :" , res)
 
 def imprimirCaixa(num):
     Tam = len(str(num))
@@ -28,5 +29,4 @@ def imprimirCaixa(num):
 
     print()
     
-imprimirCaixa(10)
-    
+main()
