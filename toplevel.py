3083d51f560ad307f87ce71a1ccbfb724f6a975a
NightlyOneV <joserobertomiranda.contato@gmail.com>
2024-10-24 11:51:58 -0300

    Update toplevel.py

--- toplevel.py
+++ toplevel.py
@@ -1,16 +1,14 @@
 import tkinter as tk 
-import time 
 
 Main = tk.Tk()
 
 def newWindow():
-    while True:
-        Secondary = tk.Toplevel()
-        Secondary.title("New Window")
+    Secondary = tk.Toplevel()
+    Secondary.title("New Window")
+    
+    CloseButton = tk.Button(Secondary, text="Close Window", command=Secondary.destroy)
+    CloseButton.grid(row=1,column=0)
         
-        CloseButton = tk.Button(Secondary, text="Close Window", command=Secondary.destroy)
-        CloseButton.grid(row=1,column=0)
-        time.sleep(1000)
     
 Button = tk.Button(master=Main, text="Go to the next window.", command=newWindow)
 Button.grid(row=2,column=0)
